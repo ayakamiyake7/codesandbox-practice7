7178f003ef0011fe01dba60856142e5c96e074e6
ayakamiyake7 <88525743+ayakamiyake7@users.noreply.github.com>
2021-12-26 13:14:12 +0000

    Practice typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
 
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
+const user: User = {
+  name: "Ayaka"
+  // hobbies: ["Reading", "Game"]
 };
 
 export default function App() {
@@ -21,10 +23,17 @@ export default function App() {
   };
   return (
     <div className="App">
+      <Text color="red" fontSize="18px" />
+      <UserProfile user={user} />
       <button onClick={onClickFetchData}>Fetch data</button>
       {todos.map((todo) => (
         // <p key={todo.id}>{todo.title}</p>
-        <Todo title={todo.title} userId={todo.userId} />
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,5 +1,9 @@
-export const Todo = (props) => {
+import { VFC } from "react";
+import { TodoType } from "./types/todo";
+
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   console.log(props);
-  const { title, userId } = props;
-  return <p>{`${title}(User: ${userId})`}</p>;
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[Done]" : "[Not Yet]";
+  return <p>{`${completeMark} ${title}(User: ${userId})`}</p>;
 };
